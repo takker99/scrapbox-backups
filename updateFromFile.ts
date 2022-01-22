6b3890a1506282a3cceddf2c808e7b3a7fc2f857
takker99 <37929109+takker99@users.noreply.github.com>
2022-01-22 11:41:20 +0900

    1ページずつばらすscriptを作った

---
+++ updateFromFile.ts
@@ -0,0 +1,72 @@
+import { ensureDir } from "https://deno.land/std@0.122.0/fs/mod.ts";
+import { fromFileUrl } from "https://deno.land/std@0.122.0/path/mod.ts";
+import { pooledMap } from "https://deno.land/std@0.122.0/async/mod.ts";
+import type {
+  ExportedData,
+  ExportedPage,
+} from "https://raw.githubusercontent.com/scrapbox-jp/types/0.0.8/api/response.ts";
+
+const url = new URL(Deno.args[0], import.meta.url);
+const res = await fetch(url);
+const json = (await res.json()) as ExportedData<true>;
+const { pages, name, displayName } = json;
+
+function toPageURL(encodedTitle: string) {
+  return new URL(`./${encodeURIComponent(encodedTitle)}.json`, dir);
+}
+
+// directoryがなければ作る
+const dir = new URL(`./${name}/pages/`, import.meta.url);
+ensureDir(fromFileUrl(dir));
+
+// 今direcotoryにあるjsonファイルのうち、削除するものを列挙する
+const titles = pages.map((page) => page.title.replaceAll("/", "%2F"));
+const oldTitles = [] as string[];
+for await (const { name, isFile } of Deno.readDir(dir)) {
+  if (!isFile) continue;
+  if (!name.endsWith(".json")) continue;
+  const title = name.slice(0, -5);
+  if (titles.includes(title)) continue;
+  oldTitles.push(title);
+}
+
+const results = pooledMap(
+  10,
+  pages,
+  async (page) => {
+    // PATHに使えない文字をencodeする
+    const title = page.title.replaceAll("/", "%2F");
+    const url = toPageURL(title);
+    try {
+      try {
+        const text = await Deno.readTextFile(url);
+        const json = JSON.parse(text) as ExportedPage<true>;
+        if (page.updated === json.updated) return;
+      } catch (e: unknown) {
+        if (!(e instanceof Deno.errors.NotFound)) throw e;
+      }
+      await Deno.writeTextFile(
+        toPageURL(title),
+        JSON.stringify(page, null, 2),
+      );
+    } catch (e: unknown) {
+      console.error(e);
+    }
+    return title;
+  },
+);
+let count = 0;
+for await (const title of results) {
+  if (title === undefined) continue;
+  console.log(`[${++count}/${pages.length}]Expand "${title}"`);
+}
+let count2 = 0;
+for (const title of oldTitles) {
+  console.log(`[${++count2}/${oldTitles.length}]Delete "${title}"`);
+  await Deno.remove(toPageURL(title));
+}
+await Deno.writeTextFile(
+  new URL(`../project.json`, dir),
+  JSON.stringify({ name, displayName }, null, 2),
+);
+console.log(`Finish updating: ${count} Updated, ${count2} Removed`);
